3e954a1c52b44eed2faf363956cd2116caa24c48
swapnil1726@gmail.com <swapnil1726@gmail.com>
2026-05-14 16:56:07 +0530

    Updated_dataset

--- Lisbon_crime_scenes/MATLAB/GEOsuccesscode2.m
+++ Lisbon_crime_scenes/MATLAB/GEOsuccesscode2.m
@@ -1,6 +1,15 @@
 
-numericData = table2array(Dataset1(:, {'mway_dist', 'police_dis'}));
+numericData = table2array(CompleteData20241(:, {'mway_dist', 'police_dis'}));
 
 inputdata = numericData(:,1:2);
 %outputdata = numericData(:,3);
 %Dataset = Dataset(:,8:9);
+
+
+% Extract the 'success_attack' column from the table
+numericData = table2array(CompleteData20241(:, 'success_attack'));
+
+% Convert the extracted column to double format (if not already in double format)
+outputData = double(numericData);
+
+% Now 'outputData' can be used as input for your fuzzy inference system
